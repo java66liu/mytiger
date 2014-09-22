83bb86bd11a1f88d9cad0c23d0b1231a69bc3dda
任泽华 <renzholy@gmail.com>
2014-09-22 20:56:48 +0800

    chapter5 complete

---
+++ chapter6/test.sh
@@ -0,0 +1,22 @@
+#/bin/bash
+
+if [ -n "$1" ]; then 
+	prog=$1
+else
+	prog=a.out
+fi
+
+for i in {1..49};
+do
+	echo $i
+	./$prog	../testcases/test$i.tig
+	echo "----------------------"
+done
+
+echo "merge"
+./$prog	../testcases/merge.tig
+echo "----------------------"
+
+echo "queen"
+./$prog	../testcases/queens.tig
+echo "----------------------"
